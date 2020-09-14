d54ede47f2281d6fe86afda1d65f44bf16452a01
Mister-Webdev <roman.pogo@yandex.ru>
2020-09-14 20:40:08 +0600

    JS Tasks

--- FlattenNestedArray.js
+++ FlattenNestedArray.js
@@ -10,9 +10,35 @@ flattenArray([1, [2], [3, [[4]]]]) // -> [1, 2, 3, 4]
 
 //SOLUTION 2:
 const flattenArray = (val, flatArr = []) => {
-  val.forEach((item) => {
-    if (Array.isArray(item)) flattenArray(item, flatArr)
-    else flatArr.push(item)
-  })
+  val.forEach((item) =>
+    Array.isArray(item) ? flattenArray(item, flatArr) : flatArr.push(item)
+  )
   return flatArr
 }
+
+//SOLUTION 3:
+const arr = [1, 2, [3, 4, [5, 6]]]
+
+const flatDeep = (arr, d = 1) => {
+  return d > 0
+    ? arr.reduce(
+        (acc, val) =>
+          acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val),
+        []
+      )
+    : arr.slice()
+}
+
+flatDeep(arr, Infinity) // -> [1, 2, 3, 4, 5, 6]
+
+//SOLUTION 4:
+const flatten = (arr, result = []) => {
+  arr.forEach((i) =>
+    Array.isArray(i) ? result.push(...flatten(i)) : result.push(i)
+  )
+  return result
+}
+
+const nested = [1, 2, 3, [4, 5, [6, 7], 8, 9]]
+
+flatten5(nested) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
---
+++ Range.js
@@ -0,0 +1,16 @@
+const range = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
+
+// range 0..4
+range(0, 4, 1)
+// [0, 1, 2, 3, 4]
+
+// range 1..10 with step of 2
+range(1, 10, 2)
+// [1, 3, 5, 7, 9]
+
+// Generate the alphabet using Array.from making use of it being ordered as a sequence
+range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map((x) =>
+  String.fromCharCode(x)
+)
+// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
