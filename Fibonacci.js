f1b708c7f0000e61d00ca11684609c6a29d51fdb
Mister-webdev <roman.pogo@yandex.ru>
2021-06-01 04:55:00 +0600

    refactoring

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,21 +1,8 @@
-//SOLUTION 1:
-const fibonacci = (n) => (n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2))
-
-fibonacci(10)
-
-//SOLUTION 2:
-const fibonacci1 = (n) => {
-  if (n <= 2) return 1
-  return fibonacci1(n - 1) + fibonacci1(n - 2)
-}
-
-fibonacci1(14)
-
-//SOLUTION 3:
-const fibonacci2 = (n) => {
+const fibonacci = n => {
   let a = 1
   let b = 0
   let c = 0
+
   while (n > 0) {
     c = a + b
     b = a
@@ -25,4 +12,6 @@ const fibonacci2 = (n) => {
   return b
 }
 
-fibonacci2(14)
+console.log(
+  fibonacci(1000)
+)
