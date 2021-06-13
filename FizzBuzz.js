fc8636ea0c42a4d9b3205282d850b41c8d18462e
Mister-webdev <roman.pogo@yandex.ru>
2021-06-13 21:29:40 +0600

    refactor

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,8 @@
-const fizzBuzz = num => {
-  const check = (a, b) => a % b === 0
-
-  for (let i = 1; i <= num; i += 1) {
-    !check(i, 5) && !check(i, 3) && console.log(i)
-      || check(i, 3) && check(i, 5) && console.log("fizz buzz")
-      || check(i, 3) && console.log("fizz")
-      || check(i, 5) && console.log("buzz")
+const fizzBuzz = (num, i = 1) => {
+  while (i < num) {
+    let str = `${++i % 3 ? '' : 'Fizz'}${i % 5 ? '' : 'Buzz'}`
+    console.log(str || i)
   }
-  return num
 }
 
-console.log(fizzBuzz(20))
+fizzBuzz(20)
