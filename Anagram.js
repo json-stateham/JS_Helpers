766e92b350f0784ccacb16ba24238671763dc680
Mister-webdev <roman.pogo@yandex.ru>
2021-06-21 02:25:46 +0600

    Anagram

---
+++ Anagram.js
@@ -0,0 +1,45 @@
+// 1
+const isAnagram1 = (a, b) => {
+  if (typeof a !== 'string' || typeof b !== 'string') {
+    console.warn('Arguments must be a string')
+  }
+
+  const sorted = x =>
+    [...x.replace(/\W/g, '').toLowerCase()]
+      .sort((a, b) => a.localeCompare(b))
+      .join('')
+
+  return sorted(a) === sorted(b)
+}
+
+console.log(isAnagram1('finder', 'Fr * ;iend')) //true
+console.log(isAnagram1('bye', 'Hi')) //false
+
+// 2
+const isAnagram = (a, b) => {
+  if (typeof a !== 'string' || typeof b !== 'string') {
+    console.warn('Arguments must be a string')
+  }
+
+  const prepared = str => [...str.replace(/\W/g, '').toLowerCase()]
+
+  const countHash = (param) =>
+    prepared(param).reduce((acc, el) => {
+      acc[el] = acc[el] + 1 || 1
+      return acc
+    }, {})
+
+  const countedA = countHash(a)
+  const countedB = countHash(b)
+
+  if (Object.keys(countedA).length !== Object.keys(countedB).length) return false
+
+  for (let key in countedA) {
+    if (countedA[key] !== countedB[key]) return false
+  }
+
+  return true
+}
+
+console.log(isAnagram1('finder', 'Fr * ;iend')) //true
+console.log(isAnagram1('bye', 'Hi')) //false
