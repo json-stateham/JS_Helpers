0465850378c159d49fdee245c3f91f5f7a3675f8
Mister-webdev <roman.pogo@yandex.ru>
2021-06-24 04:34:18 +0600

    array chunkify

--- ArrayChunkify.js
+++ ArrayChunkify.js
@@ -1,8 +1,23 @@
-//a huge array of 99 items I need to split into groups of 10
+// 1
+const chunkify1 = (array, size) => {
+  const result = []
 
-const hugeArray = Array.from({ length: 99 }, (_, i) => i)
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, size + i))
+  }
 
-const chunkify = (array, chunkSize = 10) => {
+  return result
+}
+
+const data = [1, 2, 3, 4, 5, 6, 7]
+console.log(chunkify1(data, 2)) // [[1, 2], [3, 4], [5, 6], [7]]
+console.log(chunkify1(data, 3)) // [[1, 2, 3], [4, 5, 6], [7]]
+
+
+//2
+const hugeArray = Array.from({ length: 10 }, (_, i) => i)
+
+const chunkify2 = (array, chunkSize = 2) => {
   const chunks = Array.from(
     { length: Math.ceil(array.length / chunkSize) },
     (_, i) => {
@@ -13,4 +28,4 @@ const chunkify = (array, chunkSize = 10) => {
   return chunks
 }
 
-console.log(chunkify(hugeArray))
+console.log(chunkify2(hugeArray))
