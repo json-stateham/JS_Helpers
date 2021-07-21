b21f096432af1f57b6fe5ecf643b52c3b71cf4c4
Mister-webdev <roman.pogo@yandex.ru>
2021-07-21 23:14:51 +0600

    Check Parenthesis

--- CheckParenthesis.js
+++ CheckParenthesis.js
@@ -1,4 +1,5 @@
-const checkParenthesis = str => {
+// 1
+const checkParenthesis1 = str => {
   const charHash = {
     "(": ")",
     "{": "}",
@@ -21,10 +22,38 @@ const checkParenthesis = str => {
 }
 
 console.log(
-  checkParenthesis("()"), // true
-  checkParenthesis("(}"), // false
-  checkParenthesis("([])"), // true
-  checkParenthesis("(()"), // false
-  checkParenthesis("[([])]"), // true
-  checkParenthesis("([)]") // false
+  checkParenthesis1("()"), // true
+  checkParenthesis1("(}"), // false
+  checkParenthesis1("([])"), // true
+  checkParenthesis1("(()"), // false
+  checkParenthesis1("[([])]"), // true
+  checkParenthesis1("([)]") // false
+)
+
+
+// 2
+const checkParenthesis2 = str => {
+  const charHash = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  }
+
+  return [...str].reduce((stack, char) => {
+    if (charHash[char]) {
+      stack.push(char)
+    } else if (char !== charHash[Array.prototype.pop.call(stack)]) {
+      return false
+    }
+    return stack
+  }, []).length === 0
+}
+
+console.log(
+  checkParenthesis2("()"), // true
+  checkParenthesis2("(}"), // false
+  checkParenthesis2("([])"), // true
+  checkParenthesis2("(()"), // false
+  checkParenthesis2("[([])]"), // true
+  checkParenthesis2("([)]") // false
 )
