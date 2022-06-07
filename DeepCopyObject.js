e94dbe8b7d063612296ea71c9f99bce9416ce881
json_stateham <roman.pogo@yandex.ru>
2022-06-08 03:46:00 +0600

    deep copy object

--- DeepCopyObject.js
+++ DeepCopyObject.js
@@ -1,6 +1,6 @@
 const deepCopy = obj => {
   let aux = obj;
-  if (!!obj && obj.constructor.name === 'Object') {
+  if (Boolean(obj) && obj.constructor.name === 'Object') {
     aux = new obj.constructor();
     Object.getOwnPropertyNames(obj).forEach(
       prop => (aux[prop] = deepCopy(obj[prop]))
