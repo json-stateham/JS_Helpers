bbcaaecd948626a7d7458e104088fdb05dabdd87
json_stateham <roman.pogo@yandex.ru>
2022-08-20 22:09:00 +0600

    check parenthesis

--- Interview_Tasks/CheckParenthesis.js
+++ Interview_Tasks/CheckParenthesis.js
@@ -1,59 +1,25 @@
-// 1
-const checkParenthesis1 = str => {
-  const charHash = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-  }
+const checkParenthesis = parenthesis => {
+  const parenthesisMap = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
 
-  const stack = []
-  let i = -1
+  const stack = [];
 
-  while (++i < str.length) {
-    let char = str[i]
-    if (charHash[char]) {
-      stack.push(char)
-    } else if (char !== charHash[stack.pop()]) {
-      return false
+  for (const char of parenthesis) {
+    if (parenthesisMap[char]) {
+      stack.push(char);
+    } else if (char !== parenthesisMap[stack.pop()]) {
+      return false;
     }
   }
 
-  return stack.length === 0
-}
+  return stack.length === 0;
+};
 
-console.log(
-  checkParenthesis1("()"), // true
-  checkParenthesis1("(}"), // false
-  checkParenthesis1("([])"), // true
-  checkParenthesis1("(()"), // false
-  checkParenthesis1("[([])]"), // true
-  checkParenthesis1("([)]") // false
-)
-
-
-// 2
-const checkParenthesis2 = str => {
-  const charHash = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-  }
-
-  return [...str].reduce((stack, char) => {
-    if (charHash[char]) {
-      stack.push(char)
-    } else if (char !== charHash[Array.prototype.pop.call(stack)]) {
-      return false
-    }
-    return stack
-  }, []).length === 0
-}
-
-console.log(
-  checkParenthesis2("()"), // true
-  checkParenthesis2("(}"), // false
-  checkParenthesis2("([])"), // true
-  checkParenthesis2("(()"), // false
-  checkParenthesis2("[([])]"), // true
-  checkParenthesis2("([)]") // false
-)
+console.log(checkParenthesis('(}')); // false
+console.log(checkParenthesis('([])')); // true
+console.log(checkParenthesis('[{]}')); // false
+console.log(checkParenthesis('(()')); // false
+console.log(checkParenthesis('[([])]')); // true
