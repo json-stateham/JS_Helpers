26b90ac0acce95d6ce9e1aa21fd484af414ca0b7
json_stateham <roman.pogo@yandex.ru>
2022-08-20 22:51:30 +0600

    fizzBuzz

--- Interview_Tasks/FizzBuzz.js
+++ Interview_Tasks/FizzBuzz.js
@@ -1,15 +1,7 @@
-const fizzBuzz1 = (num, i = 0) => {
-  while (i < num)
-    console.log(`${++i % 3 ? '' : 'Fizz'}${i % 5 ? '' : 'Buzz'}` || i);
-};
-
-fizzBuzz1(20);
-
-const fizzBuzz2 = num => {
-  for (let i = 1; i < num; i += 1) {
-    if (i % 15 == 0) console.log('FizzBuzz');
-    else if (i % 3 == 0) console.log('Fizz');
-    else if (i % 5 == 0) console.log('Buzz');
-    else console.log(i);
+const fizzBuzz = (num, i = 0) => {
+  while (i++ < num) {
+    const fizz = i % 3 ? '' : 'Fizz';
+    const buzz = i % 5 ? '' : 'Buzz';
+    console.log(`${fizz}${buzz}` || i);
   }
 };
