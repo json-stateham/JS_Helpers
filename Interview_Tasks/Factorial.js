f290a696852f52269e9e61a413ceb5351365b477
json_stateham <roman.pogo@yandex.ru>
2022-08-20 22:16:45 +0600

    factorial

--- Interview_Tasks/Factorial.js
+++ Interview_Tasks/Factorial.js
@@ -1,11 +1,10 @@
 const factorial = n => {
-  if (n === 0) {
-    return 1
-  } else {
-    const aux = factorial(n - 1)
-    return n * aux
+  if (n === 0) return 1;
+  else {
+    const aux = factorial(n - 1);
+    return n * aux;
   }
-}
+};
 
-const fact100 = factorial(100)
-console.log(fact100)
+console.log(factorial(5)); // 120
+console.log(factorial(10)); // 3628800
