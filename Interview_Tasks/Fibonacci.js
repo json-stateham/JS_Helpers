d4762b4702f42e3dff21aec06cdefcd33ebee56a
json_stateham <roman.pogo@yandex.ru>
2022-08-20 22:22:25 +0600

    fibonacci

--- Interview_Tasks/Fibonacci.js
+++ Interview_Tasks/Fibonacci.js
@@ -1,22 +1,22 @@
 // 1
-const fibbo = (n, a = 0, b = 1) => (n === 0 ? a : fibbo(n - 1, b, a + b))
+const fib = (n, a = 0, b = 1) => (n === 0 ? a : fib(n - 1, b, a + b));
 
-console.log(fibbo(999))
-
+console.log(fib(999));
 
 // 2
 const fibonacci = n => {
-  let a = 1
-  let b = 0
-  let c = 0
+  let a = 1;
+  let b = 0;
+  let c = 0;
 
   while (n > 0) {
-    c = a + b
-    b = a
-    a = c
-    n--
+    c = a + b;
+    b = a;
+    a = c;
+    n--;
   }
-  return b
-}
+  
+  return b;
+};
 
-console.log(fibonacci(999))
+console.log(fibonacci(999));
