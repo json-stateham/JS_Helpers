a95f8d09a8aa231b2033aeb0754e162b23829b96
json_stateham <roman.pogo@yandex.ru>
2022-08-20 22:39:04 +0600

    twoNumbersEqualsTarget

--- Interview_Tasks/findTwoSum.js
+++ Interview_Tasks/findTwoSum.js
@@ -1,18 +1,17 @@
-// Given an array of numbers [1,2,3,4,5] and a number target 9.
-// Return indices of the two numbers such that they add up to target or return -1.
+// Given an array of numbers [5, 1, 2, 4, 3] and a target number 9.
+// Return two numbers so that their sum equals the target, or return -1.
 
-const arr = [1, 2, 3, 4, 5];
-
-const findTwoSum = (arr, target) => {
+const twoNumbersEqualsTarget = (arr, target) => {
   const hash = new Map();
 
   for (let i = 0; i < arr.length; i += 1) {
-    const diff = target - arr[i];
-    if (hash.has(diff)) return [i, hash.get(diff)];    
-    hash.set(arr[i], i);
+    const current = arr[i];
+    const diff = target - current;
+    if (hash.has(diff)) return [current, diff];
+    hash.set(current, i);
   }
-  
-  return -1;
-}
 
-console.log(findTwoSum(arr, 9)) // [4, 3]
+  return -1;
+};
+
+console.log(twoNumbersEqualsTarget([5, 1, 2, 4, 3], 9)); // [4, 5]
