ef232d2a5396717f72b8fd2cbdbccc731a83733b
json_stateham <roman.pogo@yandex.ru>
2023-01-15 02:25:23 +0600

    refactoring Fibonacci

--- Algorithms/Fibonacci.js
+++ Algorithms/Fibonacci.js
@@ -4,19 +4,17 @@ const fib = (n, a = 0, b = 1) => (n === 0 ? a : fib(n - 1, b, a + b));
 console.log(fib(999));
 
 // iterative
-const fibonacci = n => {
-  let a = 1;
-  let b = 0;
-  let c = 0;
+const fibonacci = limit => {
+  let prev = 1n, next = 0n, swap;  
 
-  while (n > 0) {
-    c = a + b;
-    b = a;
-    a = c;
-    n--;
+  while (limit) {
+    swap = prev;
+    prev = prev + next;
+    next = swap;
+    limit--;
   }
   
-  return b;
+  return next;
 };
 
 console.log(fibonacci(999));
