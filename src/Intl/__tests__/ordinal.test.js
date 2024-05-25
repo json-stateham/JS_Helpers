9b31e5b78b56755e3a5d2baa62b01dbbeaae7335
json_sateham <roman.pogo@yandex.ru>
2024-05-26 02:28:45 +0800

    add jsdoc to ordinal

--- src/Intl/__tests__/ordinal.test.js
+++ src/Intl/__tests__/ordinal.test.js
@@ -3,9 +3,9 @@ import { ordinal } from '../ordinal';
 
 describe('ordinal', () => {
   test('should return the ordinal number', () => {
-    expect(ordinal(1)).toBe('1st')
-    expect(ordinal(2)).toBe('2nd')
-    expect(ordinal(3)).toBe('3rd')
-    expect(ordinal(4)).toBe('4th')
+    expect(ordinal(1)).toBe('1st');
+    expect(ordinal(2)).toBe('2nd');
+    expect(ordinal(3)).toBe('3rd');
+    expect(ordinal(4)).toBe('4th');
   })
 })
--- src/Intl/ordinal.js
+++ src/Intl/ordinal.js
@@ -1,15 +1,22 @@
-// ordinal for numbers - 1st, 2nd, 3rd, 4th
+/**
+ * Returns the ordinal representation of a number.
+ * @param {number} num - The number for which to generate the ordinal representation.
+ * @returns {string} The ordinal representation of the input number.
+ * @example
+ * ordinal(1); // "1st"
+ * ordinal(2); // "2nd"
+ * ordinal(3); // "3rd"
+ */
+export const ordinal = num => {
+  const ordinals = {
+    one: 'st',
+    two: 'nd',
+    few: 'rd',
+    many: 'th',
+    zero: 'th',
+    other: 'th',
+  };
 
-const ordinals = {
-  one: "st",
-  two: "nd",
-  few: "rd",
-  many: "th",
-  zero: "th",
-  other: "th"
-};
-
-export const ordinal = (num) => {
-  const plural = new Intl.PluralRules("en-US", { type: "ordinal" });
+  const plural = new Intl.PluralRules('en-US', { type: 'ordinal' });
   return `${num}${ordinals[plural.select(num)]}`;
 };
