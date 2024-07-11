2d8c54738daff43c614e3c7da73faba13fbbd013
json_sateham <roman.pogo@yandex.ru>
2024-07-12 01:14:10 +0800

    format code

--- src/Number/reverseNumber.ts
+++ src/Number/reverseNumber.ts
@@ -16,10 +16,12 @@ export const reverseNumber = (num: number): number => {
   while (num !== 0) {
     // get the last digit of a number by taking the remainder of dividing the number by ten.
     const right = num % 10;
+
     // get the "left" part by leaving only the integer part from dividing the number by 10.
     num = Math.floor(num / 10);
+
     // To add the right digit to result, we need to make the added digit occupy the next digit.
-    // To do this, we need to multiply res by 10.
+    // To do this, we need to multiply result by 10.
     result = result * 10 + right;
   }
 
